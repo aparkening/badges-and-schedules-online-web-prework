288103bed431ad0315775fcb833f93469a766df3
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 20:06:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 
 # Take an array of names and return array of room assignments in the form of: "Hello, _____! You'll be assigned to room _____!"
 def assign_rooms(names)
-  names.each_index {|room| "Hello, #{names[room]}! You'll be assigned to room #{room}" }
+  names.collect {|name| "Hello, #{name}! You'll be assigned to room #{name.index_of}" }
 end
 
   describe '#printer' do
