73819cfec746eccf32c67e42f7fd60f3dd0902c1
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 19:53:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,10 +6,7 @@ end
 
 # Take an array names and return an array of badge messages 
 def batch_badge_creator(array)
-  def square_array(array)
-  array.collect { |array_value| array_value ** 2 }
-end
-  
+  array.collect { |array_value| badge_maker(array_value) }
 end
 
 
